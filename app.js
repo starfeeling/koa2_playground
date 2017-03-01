5f8b1f42667b4b1641a5ae91a606bf5741f9dfd5
Chiu ChiHong <tw7869@gmail.com>
2017-03-01 18:26:03 +0800

    先下班，明天繼續

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const app = new Koa()
+const router = require('./routers/index')
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.use(async(ctx, next) => {
     const start = new Date();
@@ -8,8 +11,4 @@ app.use(async(ctx, next) => {
     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
-app.use(ctx => {
-    ctx.body = 'Hello Koa in app-async.js';
-});
-
-app.listen(3001);
+app.listen(3001)
