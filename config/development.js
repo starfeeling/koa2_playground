e454ca38ef018c044df50dd0af55c62ff217406e
Archer Chiu <tw7869@gmail.com>
2018-04-03 11:36:18 +0800

    add sentry stack-trace package

--- config/development.js
+++ config/development.js
@@ -8,4 +8,5 @@ module.exports = {
     mongodb_url: '',
     redis_url: '',
     redis_port: '',
+    sentry_dSN: 'https://3bbac6e2709d48e8b93704e73d69c617:f04b2f8668fe49c5b10730dbed4d5ad0@sentry.io/1032206',
 };
